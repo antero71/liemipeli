821db83fede6ff2a3af550c8510af2551f6fc8df
Antero Oikkonen <antero.oikkonen@gmail.com>
2016-04-30 11:09:30 +0300

    eläimiä ja free mooncalculator ja testit

---
+++ src/main/java/com/antero/liemipeli/domain/Vampyyri.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.antero.liemipeli.domain;
+
+/**
+ *
+ * @author Antero Oikkonen
+ */
+public class Vampyyri extends ElavaOlio{
+    // tarvitsee elääkseen verta säännöllisesti
+}
