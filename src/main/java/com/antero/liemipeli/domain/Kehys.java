00db72fd8cb26c4ae6da511e8b804d67757da064
Antero Oikkonen <antero.oikkonen@gmail.com>
2016-10-30 21:09:50 +0200

    aloittelin graafista käyttöliittymää, kuva on Anjan piirtämä

---
+++ src/main/java/com/antero/liemipeli/domain/Kehys.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.antero.liemipeli.domain;
+
+import com.antero.liemipeli.ui.gui.ImagePanel;
+import java.awt.Container;
+import java.awt.TextField;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Antero Oikkonen
+ */
+public class Kehys extends JFrame {
+
+
+    private static void createAndShowGUI() {
+
+        //Create and set up the window.
+        JFrame frame = new JFrame("Liemipeli");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ImagePanel.setComponentToPane(frame.getContentPane());
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+}
