9a8abb507c60358d56a2ea233a7503e9112c395a
Antero Oikkonen <antero.oikkonen@gmail.com>
2019-11-10 15:16:59 +0200

    add some ideas to the plan.md

--- liemipeli-kotlin/src/test/kotlin/com/antero/liemipeli/domain/paikat/VarastoTest.kt
+++ liemipeli-kotlin/src/test/kotlin/com/antero/liemipeli/domain/paikat/VarastoTest.kt
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.antero.liemipeli.domain.paikat
 
 import com.antero.liemipeli.domain.TuoteImpl
@@ -18,7 +13,7 @@ import java.util.*
  */
 class VarastoTest {
 
-    private var varasto: Varasto? = null
+    lateinit var varasto: Varasto
 
     @BeforeEach
     fun setUp() {
@@ -28,52 +23,52 @@ class VarastoTest {
     @Test
     fun testHaeTuote() {
         val nimi = "jäkälä"
-        varasto!!.lisaaTuote(TuoteImpl(nimi, 10, Properties()))
-        val t = varasto!!.haeTuote(nimi)
+        varasto.lisaaTuote(TuoteImpl(nimi, 10, Properties()))
+        val t = varasto.haeTuote(nimi)
         assertTrue(t?.nimi == nimi)
     }
 
     @Test
     fun testHaeTuoteJotaeiole() {
-        assertNull(varasto!!.haeTuote("Musketti"))
+        assertNull(varasto.haeTuote("Musketti"))
     }
 
     @Test
     fun testLisaaTuoteTayteenVarastoon() {
-        assertTrue(varasto!!.lisaaTuote(TuoteImpl("yritti", 10, Properties())))
-        assertFalse(varasto!!.lisaaTuote(TuoteImpl("jäkälä", 10, Properties())))
+        assertTrue(varasto.lisaaTuote(TuoteImpl("yritti", 10, Properties())))
+        assertFalse(varasto.lisaaTuote(TuoteImpl("jäkälä", 10, Properties())))
     }
 
     @Test
     fun testKokoaEivoiasettaaPienemmaksiKuinVarastosaldo() {
-        varasto!!.asetaVarastonKoko(10)
-        varasto!!.lisaaTuote(TuoteImpl("sokeri", 10, Properties()))
-        varasto!!.lisaaTuote(TuoteImpl("vehnä", 2, Properties()))
-        varasto!!.lisaaTuote(TuoteImpl("maito", 10, Properties()))
-        assertEquals(3, varasto!!.asetaVarastonKoko(2))
+        varasto.asetaVarastonKoko(10)
+        varasto.lisaaTuote(TuoteImpl("sokeri", 10, Properties()))
+        varasto.lisaaTuote(TuoteImpl("vehnä", 2, Properties()))
+        varasto.lisaaTuote(TuoteImpl("maito", 10, Properties()))
+        assertEquals(3, varasto.asetaVarastonKoko(2))
 
     }
 
     @org.junit.jupiter.api.Test
     fun testVarastonArvo() {
-        varasto!!.asetaVarastonKoko(5)
-        varasto!!.lisaaTuote(TuoteImpl("Vehna", 25, Properties()))
-        varasto!!.lisaaTuote(TuoteImpl("sokeri", 15, Properties()))
-        varasto!!.lisaaTuote(TuoteImpl("ruuti", 100, Properties()))
+        varasto.asetaVarastonKoko(5)
+        varasto.lisaaTuote(TuoteImpl("Vehna", 25, Properties()))
+        varasto.lisaaTuote(TuoteImpl("sokeri", 15, Properties()))
+        varasto.lisaaTuote(TuoteImpl("ruuti", 100, Properties()))
 
-        assertEquals(140, varasto!!.laskeVarastonArvo())
+        assertEquals(140, varasto.laskeVarastonArvo())
 
     }
 
     @Test
     fun testVarastonArvoHaeTuote() {
-        varasto!!.asetaVarastonKoko(5)
-        varasto!!.lisaaTuote(TuoteImpl("Vehna", 25, Properties()))
-        varasto!!.lisaaTuote(TuoteImpl("sokeri", 15, Properties()))
-        varasto!!.lisaaTuote(TuoteImpl("ruuti", 100, Properties()))
-        val t = varasto!!.haeTuote("ruuti")
+        varasto.asetaVarastonKoko(5)
+        varasto.lisaaTuote(TuoteImpl("Vehna", 25, Properties()))
+        varasto.lisaaTuote(TuoteImpl("sokeri", 15, Properties()))
+        varasto.lisaaTuote(TuoteImpl("ruuti", 100, Properties()))
+        val t = varasto.haeTuote("ruuti")
         assertEquals("ruuti", t?.nimi)
-        assertEquals(40, varasto!!.laskeVarastonArvo())
+        assertEquals(40, varasto.laskeVarastonArvo())
         assertEquals(100, t?.hinta
         )
 
@@ -83,28 +78,28 @@ class VarastoTest {
     fun testAsetaVarastonKokoNegatiiviseksi() {
 
         val exception = assertThrows(IllegalArgumentException::class.java) {
-            varasto?.asetaVarastonKoko(-10) }
+            varasto.asetaVarastonKoko(-10) }
         assertEquals("koko pitää olla positiivinen", exception.message)
     }
 
     @Test
     fun asetaVarastonKoko100() {
-        assertEquals(100, varasto!!.asetaVarastonKoko(100))
+        assertEquals(100, varasto.asetaVarastonKoko(100))
     }
 
     @Test
     fun testVarastoSaldo() {
-        varasto!!.asetaVarastonKoko(10)
+        varasto.asetaVarastonKoko(10)
 
-        varasto!!.lisaaTuote(TuoteImpl("miekka", 1000, Properties()))
-        varasto!!.lisaaTuote(TuoteImpl("haarniska", 2000, Properties()))
-        varasto!!.lisaaTuote(TuoteImpl("kypärä", 1000, Properties()))
+        varasto.lisaaTuote(TuoteImpl("miekka", 1000, Properties()))
+        varasto.lisaaTuote(TuoteImpl("haarniska", 2000, Properties()))
+        varasto.lisaaTuote(TuoteImpl("kypärä", 1000, Properties()))
 
-        assertEquals(3, varasto!!.varastoSaldo())
+        assertEquals(3, varasto.varastoSaldo())
 
-        varasto!!.haeTuote("miekka")
+        varasto.haeTuote("miekka")
 
-        assertEquals(2, varasto!!.varastoSaldo())
+        assertEquals(2, varasto.varastoSaldo())
     }
 
 }
---
+++ suunnitteluasiakirjoja/plan.md
@@ -0,0 +1,141 @@
+# Liemipelin hahmottelua
+
+## Sisältää
+
+## Ympäristö
+* keskieurooppalainen
+* vuoristoa
+* suota
+* tuoretta kangasmetsää
+  * uusi leveleitä
+    * tundra
+    * aavikko
+    * trooppinen
+    * sademetsä
+* ilmasto
+  * lauhkea ilmasto
+  * kohtuullisia sateita
+  * lyhyitä kuivia kausia
+
+## Paikkoja
+
+* metsä
+* vuoristo
+* järvi
+* meri
+* joki
+* joensuisto
+* ranta
+* kaivos
+* kylä
+* linna
+* maaseutu
+* kaupunki
+* 
+
+* eläimiä 
+  * liskot
+    * Lohikäärmeitä
+  * nisäkkäät
+    * ihmissusia
+    * 
+  * linnut
+    * varis
+    * korppi
+    * naakka
+    * varpunen
+    * korppikotka
+
+* pelaaja
+* liemiä
+  * uniliemi
+  * muodonmuutosliemi
+  * onnenliemi
+  * naamioimisliemi
+  * vahvistusliemi
+  * hidastusliemi
+  * pienennysliemi
+  * suurennusliemi
+  * vedenallahengitysliemi
+  * haavanparannusliemi
+    * eri tasoisia
+  
+* myrkkyjä
+  * hidas tappaja
+  * syövyttävä
+  * 
+
+* yrittejä
+  * mäkimeirami (oregano)
+  * meirami
+  * tilli
+  * basilika
+  * 
+
+* mausteita
+  * juustokumina
+  * kumina
+  * kurkuma
+  * neilikka
+  * kaardemumma
+
+
+* sieniä
+  * punakärpässieni
+  * valkokärpässieni
+  * herkkutatti
+  * kehnäsieni
+  * kanttarelli
+  * suppilovahvero
+  * seitikki
+  * 
+* malmeja
+  * rauta
+  * pronssi
+  * hopea
+  
+* työkaluja
+  * pata
+  * kauha
+  * mortteli
+  * vene
+  * airot
+  * hakku
+  * kirves
+  * lapio
+  * kuokka
+  * viikate
+  * köysi
+  * veitsi
+  * lusikka
+  * 
+
+
+huonekalu
+  * matto
+  * sänky
+  * tuoli
+  * pöytä
+  * penkki
+  * riippusängy
+  * olkipatja
+  * viltti
+  * tyyny
+  * 
+
+vaatteita
+* viitta
+* tunika
+* paita
+* housut
+* hame
+* mekko
+* takki
+* haarniska
+* kypärä
+* hanskat
+* saappaat
+* kengät
+*  sukat
+
+  
